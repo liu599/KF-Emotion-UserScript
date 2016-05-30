bc56e0870aea00368d2ed3f789d0b490699e5c42
eddie32 <liu599@users.noreply.github.com>
2016-05-30 17:52:49 +0800

    增加反向代理服务器

--- kf.js
+++ kf.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name       绯月表情增强插件
 // @namespace  
-// @version     2.8.9
+// @version     2.9.0
 // @description KF论坛专用的回复表情, 插图扩展插件, 在发帖时快速输入自定义表情和论坛BBCODE
 // @icon        https://blog.nekohand.moe/archive/favicon.ico
 // @homepage    https://greasyfork.org/zh-CN/scripts/5124-%E7%BB%AF%E6%9C%88%E8%A1%A8%E6%83%85%E5%A2%9E%E5%BC%BA%E6%8F%92%E4%BB%B6
@@ -11,6 +11,7 @@
 // @match       https://sstmlt.net/*
 // @match       http://www.mmy.moe/*
 // @match       http://www.mddmm.com/*
+// @match       http://*miaola.info/*
 // @copyright   2014-2016, eddie32
 // @grant       none
 // @license     MIT
