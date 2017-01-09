eea0485963d34b891b678f0d92164a8fcf4e6431
eddie32 <460512944@qq.com>
2017-01-09 11:28:26 +0800

    resolve error message

--- dist/kf.js
+++ dist/kf.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name       绯月表情增强插件
 // @namespace   https://greasyfork.org/users/5415
-// @version     4.0.0
+// @version     4.0.1
 // @author      eddie32
 // @description KF论坛专用的回复表情, 插图扩展插件, 在发帖时快速输入自定义表情和论坛BBCODE
 // @icon        https://blog.nekohand.moe/favicon.ico
