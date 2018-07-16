ebcff667ea604cf86bf7846ce2a8088afaa03dda
Tokei <liu599@users.noreply.github.com>
2018-07-17 00:19:11 +0800

    https 更新
    https 更新

--- dist/kf.js
+++ dist/kf.js
@@ -7,9 +7,9 @@
 // @icon        https://blog.nekohand.moe/favicon.ico
 // @homepage    https://github.com/liu599/KF-Emotion-UserScript
 // @include     https://*miaola.info/*
-// @include     http://*2dkf.com/*
-// @include     http://*9moe.com/*
-// @include     http://*kfgal.com/*
+// @include     https://*2dkf.com/*
+// @include     https://*9moe.com/*
+// @include     https://*kfgal.com/*
 // @copyright   2014-2017, eddie32
 // @grant       none
 // @license     MIT
