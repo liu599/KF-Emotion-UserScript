7fbbbe1b8464f42454f03152e2b2e5ae23bb8f89
Tokei <970228409@qq.com>
2019-08-11 02:42:22 +0800

    update version 5.2.0 v2

--- dist/kf.js
+++ dist/kf.js
@@ -1,16 +1,17 @@
 // ==UserScript==
 // @name       绯月表情增强插件
 // @namespace   https://greasyfork.org/users/5415
-// @version     5.1.7
+// @version     5.2.0
 // @author      eddie32
 // @description KF论坛专用的回复表情, 插图扩展插件, 在发帖时快速输入自定义表情和论坛BBCODE
 // @icon        http://wx1.sinaimg.cn/small/62800f43ly8fttwqsdhc2j20e80e8t9b.jpg
 // @homepage    https://github.com/liu599/KF-Emotion-UserScript
 // @include     https://*miaola.info/*
+// @include     https://*ikfol.com/*
 // @include     https://*2dkf.com/*
 // @include     https://*9moe.com/*
 // @include     https://*kfgal.com/*
-// @copyright   2014-2018, eddie32
+// @copyright   2014-2019, eddie32
 // @grant       none
 // @license     MIT
 // @run-at      document-end
