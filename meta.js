027ce9a46b3f26d70a1bdb685a2fec70cf26d0a3
Tokei <liu599@users.noreply.github.com>
2023-03-09 03:27:55 +0800

    Remove website restriction

--- meta.js
+++ meta.js
@@ -6,13 +6,9 @@
 // @description KF论坛专用的回复表情, 插图扩展插件, 在发帖时快速输入自定义表情和论坛BBCODE
 // @icon        http://wx1.sinaimg.cn/small/62800f43ly8fttwqsdhc2j20e80e8t9b.jpg
 // @homepage    https://github.com/liu599/KF-Emotion-UserScript
-// @include     https://*miaola.info/*
-// @include     https://*ikfol.com/*
-// @include     https://*2dkf.com/*
-// @include     https://*9moe.com/*
-// @include     https://*kfgal.com/*
-// @include     https://*kforz.com/*
-// @copyright   2014-2021, eddie32
+// @match      *://*/*
+// @include    *
+// @copyright   2014-2023, eddie32
 // @grant       none
 // @license     MIT
 // @run-at      document-end
